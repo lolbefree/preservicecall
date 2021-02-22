b4042d4fdbbbe04d8c9f6714db87ec1acce1f8de
lolbefree <jioji1000@gmail.com>
2021-02-22 12:35:53 +0200

    Initial commit

---
+++ pscall.bat
@@ -0,0 +1,3 @@
+@echo off
+set gslid="%1"
+py -u "C:\AM\BAT\preservicecall\remote.py" %gslid%
---
+++ remote.py
@@ -0,0 +1,50 @@
+import wmi, attribute, sys, os, pyodbc, time
+
+
+def m1(f1):
+    var = f1
+    ip = attribute.ip
+    SW_SHOWNORMAL = 1
+    server = ''
+    database = ''
+    username = ''
+    password = ''
+    driver = '{SQL Server}'  # Driver you need to connect to the database
+    port = '1433'
+    cnn = pyodbc.connect(
+        'DRIVER=' + driver + ';PORT=port;SERVER=' + server + ';PORT=1443;DATABASE=' + database + ';UID=' + username +
+        ';PWD=' + password)
+    gsalid = var
+    cursor = cnn.cursor()
+
+    cursor.execute("select uwtel from gsals01 where gsalid={}".format(gsalid))
+    for row in cursor:
+        tel = row[0]
+        print(tel)
+
+    print("Establishing connection to %s" % ip)
+    # username = ""
+    password = ""
+    user_isert = ""
+
+    conn = wmi.WMI(ip, user=r"domain\{}".format(user_isert), password=password)
+    process_startup = conn.Win32_ProcessStartup.new()
+    process_startup.ShowWindow = SW_SHOWNORMAL
+    process_id, result = conn.Win32_Process.Create(
+        CommandLine="C:\Program Files\internet explorer\iexplore.exe \"http://localhost:4059/execsvcscript?name=webcall&startparam1={}&startparam2={}\"".format(
+            tel, gsalid), ProcessStartupInformation=process_startup)
+
+    if result == 0:
+        print("Process started successfully: %d" % process_id)
+
+    else:
+        raise (RuntimeError, "Problem creating process: %d" % result)
+        print(tel)
+    time.sleep(7)    
+    for process in conn.Win32_Process(ProcessId=process_id):
+        process.Terminate()
+        print(process_id, "killed")
+
+if __name__ == "__main__":
+    args = sys.argv[1]
+    get_gsalid = m1(args)
